e94ada6be175c69127fe01022859d053f6905a55
jacobbrandis <44782543+jacobbrandis@users.noreply.github.com>
2018-11-17 00:12:01 -0800

    Add files via upload

--- settings.py
+++ settings.py
@@ -5,6 +5,22 @@ HEIGHT = 600
 # frames per second
 FPS = 60
 # colors
+FONT_NAME = 'comic sans'
+
 WHITE = (255, 255, 255)
 ORANGE = (255,165,0)
 BLUE = (0,0,250)
+WHITE = (255, 255, 255)
+BLACK = (0,0,0)
+REDDISH = (240,55,66)
+# player settings
+PLAYER_ACC = 0.8
+PLAYER_FRICTION = -0.08
+PLAYER_GRAV = 0.8
+PLAYER_JUMP = 50
+PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40),
+                 (65, HEIGHT - 300, WIDTH-400, 40),
+                 (20, HEIGHT - 350, WIDTH-300, 40),
+                 (200, HEIGHT - 150, WIDTH-350, 40),
+                 (200, HEIGHT - 450, WIDTH-350, 40)]
+
